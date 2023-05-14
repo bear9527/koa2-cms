637547740daac4fcf5bfef92ef256604d71eb23f
mxf <bear2770@126.com>
2023-05-14 22:15:34 +0800

    fix: 分类优化

--- src/schema/article.ts
+++ src/schema/article.ts
@@ -10,7 +10,7 @@ const article_cate_id = joi.number().integer().min(1).required()
 const article_description = joi.string().max(255)
 const article_content = joi.string().required().allow('')
 const article_state = joi.string().valid("已发布", "草稿").required()
-const article_pic = joi.string()
+const article_pic = joi.string().allow('')
 const article_pub_date = joi.string()
 
 // 创建分类
